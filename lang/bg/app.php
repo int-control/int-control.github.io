59e45d9f66444a38769a2c98a9cbcccd2d57e1da
Pavel Grancharov <dvdtygc@gmail.com>
2022-09-04 23:24:17 +0300

    Move translations.

---
+++ lang/bg/app.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    'nav' => [
+        'home' => 'Начало',
+        'about_us' => 'За нас',
+        'activities' => 'Дейности',
+        'areas' => 'Области' ,
+        'contacts'  => 'Контакти',
+    ],
+
+    // 'products' => [
+    //     'products'    => 'Продукти',
+    //     'controllers' => 'Контролери',
+    //     'sensors'     => 'Датчици',
+    //     'scada'       => 'SCADA',
+    // ],
+    //
+    // 'services' => [
+    //     'services'      => 'Услуги',
+    //     'development'   => 'Развой',
+    //     'consultations' => 'Консултации',
+    //     'repairs'       => 'Ремонти',
+    // ],
+    //
+    // 'applications' => [
+    //     'applications'  => 'Приложения',
+    //     'mining'        => 'Миннодобивна и преработвателна промишленост',
+    //     'pharmacy'      => 'Фармацевтична',
+    //     'cable_lines'   => 'Въжени линии',
+    //     'food_industry' => 'Хранително вкусова промишленост',
+    // ],
+    //
+    // 'documents' => 'Документи',
+    // 'contacts'  => 'Контакти',
+    // ],
+
+    'home_page' => [
+        'services'     => 'Услуги',
+        'who_are_we'   => 'Кои сме ние',
+        'our_projects' => 'Нашите проекти',
+        'news' => 'Новини',
+    ],
+
+    'general' => [
+        'read_more' => 'Прочети още',
+    ],
+];
